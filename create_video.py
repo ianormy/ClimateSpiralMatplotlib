6acca6104a40db4fb2772c3965998c899712d919
Ian Ormesher <ianormy@gmail.com>
2022-03-26 13:47:14 +0000

    FEAT: initial version

--- create_video.py
+++ create_video.py
@@ -2,20 +2,14 @@ import os
 from subprocess import Popen, PIPE, STDOUT
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import pickle
-from matplotlib.offsetbox import (
-    AnchoredOffsetbox, AuxTransformBox, DrawingArea, TextArea, VPacker)
-import math
 from matplotlib.collections import LineCollection
-from matplotlib.colors import ListedColormap, BoundaryNorm
 import pandas as pd
 
 REPLAY_DIR = "D:\\Videos"
 FRAME_RATE = 48
 
 
-def segment_circle(num_segments):
+def segment_circle(num_segments: int):
     """Split a circle into num_segments segments
 
     Args:
@@ -40,8 +34,9 @@ def create_video():
     """Creates the animation video."""
     r = 7.0
 
-    months = ["Mar", "Feb", "Jan", "Dec", "Nov", "Oct", "Sep", "Aug",
-              "Jul", "Jun", "May", "Apr"]
+    # months - these go backwards and start at March
+    months = ["Mar", "Feb", "Jan", "Dec", "Nov", "Oct", "Sep", "Aug", "Jul", "Jun", "May", "Apr"]
+    # month index lookup table
     month_idx = [2, 1, 0, 11, 10, 9, 8, 7, 6, 5, 4, 3]
     radius = r + 0.4
     month_points = segment_circle(len(months))
@@ -53,22 +48,25 @@ def create_video():
     y_vals = []
     for i in range(0, len(x_orig)):
         r_pos = x_orig[i] * r_factor
-        a = month_points[month_idx[i % 12], 2]
-        x_r, y_r = (r_pos * np.cos(a), r_pos * np.sin(a))
+        x_unit_r, y_unit_r = month_points[month_idx[i % 12], :2]
+        x_r, y_r = (r_pos * x_unit_r, r_pos * y_unit_r)
         x_vals.append(x_r)
         y_vals.append(y_r)
 
-    ffmpeg = Popen(['ffmpeg', '-y', '-f', 'image2pipe',
-                    '-c:v', 'png',
-                    '-s', "640x480",
-                    '-pix_fmt', 'rgba',
-                    '-framerate', str(FRAME_RATE),
-                    '-i', '-',
-                    '-c:v', 'libx264',
-                    '-b:v', '4M',
-                    '-pix_fmt', 'yuv420p',
+    ffmpeg = Popen(['ffmpeg',
+                    '-y',  # overwrite output files without asking
+                    '-f', 'image2pipe',  # use a pipe for the input
+                    '-c:v', 'png',  # use the png encoder
+                    '-s', '640x480',  # screen dimensions
+                    '-pix_fmt', 'rgba',  # pixel format of RGBA
+                    '-framerate', str(FRAME_RATE),  # frame rate
+                    '-i', '-',  # input is from the pipe
+                    '-c:v', 'libx264',  # use the libx264 codec - H264
+                    '-b:v', '4M',  # bitrate
+                    '-pix_fmt', 'yuv420p',  # use the yuv420p pixel format
                     os.path.join(REPLAY_DIR, "climate_spiral.mp4")],
-                   stdin=PIPE, stderr=STDOUT)
+                   stdin=PIPE,
+                   stderr=STDOUT)
 
     fig, ax = plt.subplots(figsize=(14, 14))
     for i in range(len(x_vals)):
@@ -96,10 +94,10 @@ def create_video():
 
         # draw the month legends around the rim of the circle
         for j in range(0, len(months)):
-            a = month_points[j, 2]
-            x, y = (radius * np.cos(a), radius * np.sin(a))
-            a = a - 0.5 * np.pi
-            ax.text(x, y, months[j], props_months, rotation=np.rad2deg(a), )
+            x_unit_r, y_unit_r, angle = month_points[j]
+            x_radius, y_radius = (radius * x_unit_r, radius * y_unit_r)
+            angle = angle - 0.5 * np.pi
+            ax.text(x_radius, y_radius, months[j], props_months, rotation=np.rad2deg(angle), )
 
         if i > 1:
             x_seg = x_vals[:i]
